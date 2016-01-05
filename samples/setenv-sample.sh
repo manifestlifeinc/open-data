b62f8ad973c57c5c0849ad7864de565b64444d57
Raj Singh <rrsingh@us.ibm.com>
2016-01-05 10:10:07 -0500

    objectstore creds in env vars and better intro

---
+++ samples/setenv-sample.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export OBJECTSTORE_USER_ID='useridhere'
+export OBJECTSTORE_DOMAIN_ID='domainidhere'
+export OBJECTSTORE_USERNAME='usernamehere'
+export OBJECTSTORE_PASSWORD='passwordhere'
+export OBJECTSTORE_CONTAINER='containerhere'
